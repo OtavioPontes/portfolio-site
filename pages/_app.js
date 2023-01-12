19806153a1802577afe7cd184e0ff76dd403a1be
Otavio Pontes <otavio.pontes1103@gmail.com>
2023-01-12 12:32:16 -0300

    feat: ✨ add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import styles from "../public/App.css";
-
+import { Analytics } from "@vercel/analytics/react";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
